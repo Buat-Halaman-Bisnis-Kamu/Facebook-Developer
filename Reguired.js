3bcbf957d941c889654e5a5bea20142b8634bf61
Tikha <64693040+Sub-Olahoop@users.noreply.github.com>
2020-08-09 23:12:47 +0700

    Update Reguired.js

--- Reguired.js
+++ Reguired.js
@@ -4,3 +4,14 @@
       - main.js
       - require.js
 - fb.js
+require.config({
+  shim: {
+    'facebook' : {
+      exports: 'FB'
+    }
+  },
+  paths: {
+    'facebook': 'https://connect.facebook.net/en_US/sdk.js'
+  }
+})
+require(['fb']);
